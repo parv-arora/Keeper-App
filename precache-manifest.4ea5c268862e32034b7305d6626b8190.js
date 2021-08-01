83afbae45e83f614d9646d1dd389506f220d2f79
parvarora <parvarora2012@gmail.com>
2021-08-01 19:43:19 +0530

    Updates

---
+++ precache-manifest.4ea5c268862e32034b7305d6626b8190.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4902ebd240fa7cc745e04b12b7952a9",
+    "url": "/Keeper-App/index.html"
+  },
+  {
+    "revision": "7ba522b01f3c272ba267",
+    "url": "/Keeper-App/static/js/2.89e78890.chunk.js"
+  },
+  {
+    "revision": "65d3a341c1d01e0e2688",
+    "url": "/Keeper-App/static/js/main.ff4a2537.chunk.js"
+  },
+  {
+    "revision": "128fd620914684fbc76b",
+    "url": "/Keeper-App/static/js/runtime-main.113ea231.js"
+  }
+]);
