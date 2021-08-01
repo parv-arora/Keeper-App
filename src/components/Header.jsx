1c8c445ec6a119a2e80d1fc5e1acc1e79d17cd77
parvarora <parvarora2012@gmail.com>
2021-08-01 18:32:59 +0530

    intial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import DeleteIcon from '@material-ui/icons/Delete';
+import HighlightIcon from '@material-ui/icons/Highlight';
+
+function Header() {
+  return (
+    <header>
+      <h1><HighlightIcon />Keeper</h1>
+    </header>
+  );
+}
+
+export default Header;
